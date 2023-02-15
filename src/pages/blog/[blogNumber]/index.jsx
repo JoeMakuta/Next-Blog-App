67623698f60d980b735cea6a6c0d6db0e48d8ce8
Josué Makuta <makutajosue@gmail.com>
2023-02-15 18:10:57 +0100

    Style : add styles from layout component

--- src/pages/blog/[blogNumber]/index.jsx
+++ src/pages/blog/[blogNumber]/index.jsx
@@ -1,15 +1,18 @@
 import Link from "next/link";
+import Layout from "@/components/layout";
 import { useRouter } from "next/router";
 
 const SingleBlog = () => {
   const { blogNumber } = useRouter().query;
   return (
-    <div>
-      <h1>Blog {blogNumber}</h1>
-      <p>These are the details about the blog {blogNumber}</p>
-      <Link href='/'>Home</Link>
-      <Link href='/blog'>Blogs</Link>
-    </div>
+    <Layout>
+      <div>
+        <h1>Blog {blogNumber}</h1>
+        <p>These are the details about the blog {blogNumber}</p>
+        <Link href="/">Home</Link>
+        <Link href="/blog">Blogs</Link>
+      </div>
+    </Layout>
   );
 };
 
