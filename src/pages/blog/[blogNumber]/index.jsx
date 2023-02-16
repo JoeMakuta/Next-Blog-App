9420cb4b8b1d6436ba4ceb2fb56b4191f6a0d7ca
Josué Makuta <makutajosue@gmail.com>
2023-02-16 19:44:14 +0100

    Style : add _app.js and the global css file : main.css

--- src/pages/blog/[blogNumber]/index.jsx
+++ src/pages/blog/[blogNumber]/index.jsx
@@ -1,18 +1,16 @@
 import Link from "next/link";
-import Layout from "@/components/layout";
+import styles from "../../../components/singleblog.module.css";
 import { useRouter } from "next/router";
 
 const SingleBlog = () => {
   const { blogNumber } = useRouter().query;
   return (
-    <Layout>
-      <div>
-        <h1>Blog {blogNumber}</h1>
-        <p>These are the details about the blog {blogNumber}</p>
-        <Link href="/">Home</Link>
-        <Link href="/blog">Blogs</Link>
-      </div>
-    </Layout>
+    <div className={styles.container}>
+      <h1>Blog {blogNumber}</h1>
+      <p>These are the details about the blog {blogNumber}</p>
+      <Link href="/">Home</Link>
+      <Link href="/blog">Blogs</Link>
+    </div>
   );
 };
 
