46e17a4a61de20f01ebf1d5e0a26f6254d53cb01
Josué Makuta <makutajosue@gmail.com>
2023-02-19 22:56:45 +0100

    Feat : fetchUsers and display then intoo the dom

--- src/pages/users/index.jsx
+++ src/pages/users/index.jsx
@@ -1,9 +1,16 @@
 import Link from "next/link";
 
-const UsersList = () => {
+const UsersList = (props) => {
   return (
     <div>
       <h1>List of users</h1>
+      {props.users.map((user, index) => {
+        return (
+          <div key={index}>
+            <p>User {index+1}: {user.username}</p>
+          </div>
+        );
+      })}
       <Link href="/">Home</Link>
     </div>
   );
@@ -12,7 +19,11 @@ const UsersList = () => {
 export default UsersList;
 
 export const getStaticProps = async () => {
-  fetch("https://jsonplaceholder.typicode.com/users")
-    .then((data) => data.json())
-    .then((data) => console.log(data));
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+  return {
+    props: {
+      users: data,
+    },
+  };
 };
