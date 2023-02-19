c0dbc0c78779e24279e09a80be742ccd2c6e51f8
Josué Makuta <makutajosue@gmail.com>
2023-02-19 22:48:50 +0100

    Feat : Add getStaticProps to users and log the result

--- src/pages/users/index.jsx
+++ src/pages/users/index.jsx
@@ -1,11 +1,18 @@
-import Link from "next/link"
+import Link from "next/link";
 
-const UsersList  = () => {
-   return <div>
+const UsersList = () => {
+  return (
+    <div>
       <h1>List of users</h1>
-      <Link href="/" >Home</Link>
-   </div>
-}
+      <Link href="/">Home</Link>
+    </div>
+  );
+};
 
-export default UsersList
+export default UsersList;
 
+export const getStaticProps = async () => {
+  fetch("https://jsonplaceholder.typicode.com/users")
+    .then((data) => data.json())
+    .then((data) => console.log(data));
+};
