bc344f15b9eb5034d8a6701f16ea9c43f59b92f0
Josué Makuta <makutajosue@gmail.com>
2023-02-19 22:42:32 +0100

    Feat : Add home link to users page

--- src/pages/users/index.jsx
+++ src/pages/users/index.jsx
@@ -1,5 +1,10 @@
+import Link from "next/link"
+
 const UsersList  = () => {
-   return <h1>List of users</h1>
+   return <div>
+      <h1>List of users</h1>
+      <Link href="/" >Home</Link>
+   </div>
 }
 
 export default UsersList
