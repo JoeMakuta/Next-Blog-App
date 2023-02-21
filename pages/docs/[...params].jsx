4d7e7ddce02148a3f3bc4195dbb5f1dea24e7957
Josué Makuta <makutajosue@gmail.com>
2023-02-21 23:36:32 +0100

    Feat : add catch params for catch-all-routes

--- pages/docs/[...params].jsx
+++ pages/docs/[...params].jsx
@@ -1,9 +1,29 @@
-const Docs = () => {
-   return <div>
-      <h1>
-         Docs for the blog app
-      </h1>
-   </div>
-}
+import { useRouter } from "next/router";
 
-export default Docs
+const Docs = () => {
+  const { params = [] } = useRouter().query;
+
+  if (params.length === 1) {
+    return (
+      <div>
+        <h1>Docs for {params[0]}</h1>
+      </div>
+    );
+  } else if (params.length === 2) {
+    return (
+      <div>
+        <h1>
+          Docs for {params[0]} at {params[1]}
+        </h1>
+      </div>
+    );
+  }
+
+  return (
+    <div>
+      <h1>Docs for the blog app</h1>
+    </div>
+  );
+};
+
+export default Docs;
