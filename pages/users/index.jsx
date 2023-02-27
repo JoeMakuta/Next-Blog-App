755934901ffd9b7e106e0fa8cab076c9b1c6e97b
Josué Makuta <makutajosue@gmail.com>
2023-02-27 15:17:52 +0100

    Feat : refac users db

--- pages/users/index.jsx
+++ pages/users/index.jsx
@@ -21,7 +21,7 @@ const UsersList = (props) => {
 export default UsersList;
 
 export const getStaticProps = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const response = await fetch("http://localhost:4000/users");
   const data = await response.json();
   return {
     props: {
