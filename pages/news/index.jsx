1970f41b122c69697955a97b565355bca4df4698
Josué Makuta <makutajosue@gmail.com>
2023-02-28 14:19:41 +0100

    Feat : fetch data with  getServerSideProps and render

--- pages/news/index.jsx
+++ pages/news/index.jsx
@@ -1,9 +1,35 @@
-const News = () => {
-   return <div>
-      <h1>
-         The list of posts :
-      </h1>
-   </div>
-}
+import Link from "next/link";
 
-export default News
+const News = ({ news }) => {
+  return (
+    <div>
+      <h1>The list of posts :</h1>
+      <div>
+        {news.map((article) => {
+          return (
+            <div key={article.id}>
+              <h2>Article {article.id}</h2>
+              <h3>{article.title}</h3>
+              <p>{article.description}</p>
+              <p>Category : {article.category}</p>
+            </div>
+          );
+        })}
+      </div>
+      <Link href="/">Home</Link>
+    </div>
+  );
+};
+
+export default News;
+
+export const getServerSideProps = async () => {
+  const response = await fetch("http://localhost:4000/news");
+  const data = await response.json();
+
+  return {
+    props: {
+      news: data,
+    },
+  };
+};
